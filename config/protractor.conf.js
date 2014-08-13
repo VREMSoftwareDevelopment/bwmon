99ef680322eb079600d189100086003ad79f7810
VREMSoftwareDevelopment <VREMSoftwareDevelopment@gmail.com>
2014-08-13 12:57:28 -0400

    proctractor configuration changes

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -12,6 +12,17 @@ exports.config = {
 	specs: [
 		'../test/e2e/*Spec.js'
 	],
+	onPrepare: function() {
+		// Disable animations so e2e tests run more quickly
+		var disableNgAnimate = function() {
+			angular.module('disableNgAnimate', []).run(function($animate) {
+				$animate.enabled(false);
+			});
+		};
+
+		browser.addMockModule('disableNgAnimate', disableNgAnimate);
+		browser.driver.manage().window().maximize();
+	},
 	jasmineNodeOpts: {
 		onComplete: null,
 		showColors: true,
