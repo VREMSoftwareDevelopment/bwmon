ab5261fbd21dc9a4252d672fc6689e24e7dddfeb
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2015-01-01 13:51:09 -0500

    added update tools scripts

---
+++ scripts/update-global.bat
@@ -0,0 +1,8 @@
+call npm.cmd -g update grunt-cli
+call npm.cmd -g update bower
+call npm.cmd -g update grunt
+call npm.cmd -g update karma
+call npm.cmd -g update grunt-karma
+call npm.cmd -g update protractor
+call npm.cmd -g update http-server
+call webdriver-manager update --standalone
