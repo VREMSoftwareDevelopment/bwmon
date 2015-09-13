74dd425ae2dbaf28cdc2709898bea99dd4aec072
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2015-09-13 13:26:30 -0400

    modify grunt/server test shell script

--- scripts/run-grunt.sh
+++ scripts/run-grunt.sh
@@ -4,8 +4,20 @@ BASEDIR=$(dirname $0)
 SELENIUM=selenium
 HTTPSERVER=http-server
 
-gnome-terminal -x $BASEDIR/run-$SELENIUM.sh
-gnome-terminal -x $BASEDIR/run-$HTTPSERVER.sh
+if ps a | grep -v grep | grep $SELENIUM >/dev/null
+then
+  echo $SELENIUM is running.
+else
+  gnome-terminal -x $BASEDIR/run-$SELENIUM.sh
+fi
+
+if ps a | grep -v grep | grep $HTTPSERVER >/dev/null
+then
+  echo $HTTPSERVER is running.
+else
+  gnome-terminal -x $BASEDIR/run-$HTTPSERVER.sh
+fi
+
 grunt --gruntfile $BASEDIR/../config/gruntfile.js --base $BASEDIR/.. "$@"
 
 pkill -f $SELENIUM
