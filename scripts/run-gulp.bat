84170b9414961c9270cbec93a01fe4a1463ab8ba
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2015-09-15 17:16:37 -0400

    call gulp in windows

--- scripts/run-gulp.bat
+++ scripts/run-gulp.bat
@@ -8,7 +8,7 @@ taskkill /FI "Imagename eq cmd.exe" /FI "Windowtitle eq http-server - %BASE_DIR%
 start "selenium" %BASE_DIR%run-selenium
 start "http-server" %BASE_DIR%run-http-server
 
-gulp %*
+call gulp %*
 
 taskkill /FI "Imagename eq cmd.exe" /FI "Windowtitle eq selenium - %BASE_DIR%run-selenium"
 taskkill /FI "Imagename eq cmd.exe" /FI "Windowtitle eq http-server - %BASE_DIR%run-http-server"
