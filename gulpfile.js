04b7152a35f3c7341fe351cef128ad985e3e9bc5
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2015-09-17 20:51:47 -0400

    concat and uglify  js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,28 @@
 var gulp = require('gulp'),
 	plugins = require('gulp-load-plugins')(),
 
-	jshint = plugins.jshint,
-	protractor = plugins.protractor.protractor,
+	jshint = plugins['jshint'],
+	protractor = plugins['protractor'].protractor,
 //	webdriver_standalone = plugins.protractor.webdriver_standalone,
 //	webdriver_update = plugins.protractor.webdriver_update,
 
-//	concat = require('gulp-concat'),
-//	uglify = require('gulp-uglify'),
-//	rename = require('gulp-rename'),
+	concat = plugins['concat'],
+	uglify = plugins['uglify'],
+	rename = plugins['rename'],
+	header = plugins['header'],
+	sourcemaps = plugins['sourcemaps'],
+	minifyCss = plugins['minify-css'],
 
+	pkg = require('./package.json'),
 	map = require('map-stream'),
 	extend = require('node.extend'),
 	jshint_stylish = require('jshint-stylish'),
 
+	banner = '/*! <%=pkg.name%> - v<%=pkg.version%> - <%=pkg.description%> - <%=pkg.license%> - <%= pkg.homepage %> */',
+
 	files = {
 		main: {
-			src: ['app/js/*.js'],
+			src: ['app/js/*.js', '!app/js/bwmon.min.js', '!app/bwmonUsage.js'],
 			libs: [
 				'bower_components/angular/angular.min.js',
 				'bower_components/angular-route/angular-route.min.js',
@@ -34,7 +40,12 @@ var gulp = require('gulp'),
 			src: ['test/e2e/**/*.js']
 		},
 		data: ['app/bwmonUsage.js'],
-		srcignore: ['!app/js/bwmon.min.js', '!app/bwmonUsage.js']
+		build: {
+			dest: 'dist/js',
+			temp: 'bwmon.js',
+			name: 'bwmon.min.js',
+			libs: 'libs.min.js'
+		}
 	},
 	exitOnJshintError = map(function (file, cb) {
 		if (!file.jshint.success) {
@@ -81,24 +92,30 @@ gulp.task('e2e', function() {
 		.pipe(protractor({configFile: __dirname+'/config/protractor.conf.js'}))
 });
 
-//gulp.task('build', function() {
-//	return gulp
-//		.src(['app/bower_components/angular/angular.min.js',
-//					'app/bower_components/angular-route/angular-route.min.js',
-//					'app/bower_components/underscore/underscore-min.js',
-//					'app/bower_components/momentjs/min/moment.min.js',
-//					'app/bower_components/d3/d3.min.js',
-//					'app/bower_components/n3-line-chart/build/line-chart.min.js',
-//					'app/js/**/*.js',
-//					'!app/js/bwmon.min.js',
-//					'!app/bwmonUsage.js'])
-//				.pipe(concat('bwmon.js'))
-//				.pipe(gulp.dest('dist'))
-//				.pipe(rename('bwmon.min.js'))
-//				.pipe(uglify())
-//				.pipe(gulp.dest('dist'));
-//});
-//
+gulp.task('buildlibs', function() {
+	var src = [].concat(files.main.libs);
+	console.log(src);
+	return gulp
+		.src(src)
+		.pipe(concat(files.build.libs))
+		.pipe(gulp.dest(files.build.dest))
+});
+
+gulp.task('build', function() {
+	var src = [].concat(files.main.libs, files.main.src);
+	console.log(src);
+	return gulp
+		.src(src)
+		.pipe(sourcemaps.init())
+		.pipe(concat(files.build.temp, {newLine: ';'}))
+		.pipe(gulp.dest(files.build.dest))
+		.pipe(rename(files.build.name))
+		.pipe(uglify())
+		.pipe(header(banner, {pkg: pkg}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(files.build.dest));
+});
+
 
 gulp.task('watch', function() {
 	var src = [].concat(files.main.src, files.unit.src, files.e2e.src, files.data);
