835a43fd69d30088b58c8372098cd45694d2420d
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2015-09-28 20:13:24 -0400

    simplified protractor config

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -1,7 +1,6 @@
 'use strict';
 
 exports.config = {
-	seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
 	capabilities: {
 		'browserName': 'chrome'
 	},
