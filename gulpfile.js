96644e594947bceae77d121a5f6812cd6089043d
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2018-12-27 13:33:28 -0500

    added gulp logger

--- gulpfile.js
+++ gulpfile.js
@@ -16,13 +16,12 @@
 
 var gulp = require('gulp'),
 	plugins = require('gulp-load-plugins')(),
-
 	pkg = require('./package.json'),
 	extend = require('node.extend'),
 	jshint_stylish = require('jshint-stylish'),
 	del = require('del'),
+	log = require('fancy-log'),
 	argv = require('yargs').argv,
-
 	banner = 'window.VERSION="<%=pkg.version%>";\n/*\n\t<%=pkg.name%> v<%=pkg.version%>\n\t(C) 2010 - 2018 VREM Software Development\n\t<%= pkg.homepage %>\n\tLicense: <%=pkg.license%>\n*/\n',
 	srcdir = 'app',
 	dstdir = 'dist',
@@ -124,33 +123,43 @@ var gulp = require('gulp'),
 	};
 
 gulp.task('uglify:clean', function() {
+	log(dstdir);
 	return del.sync(dstdir);
 });
 
 gulp.task('templates:clean', function() {
-	return del.sync(files.templates.dstdir+files.templates.name);
+	var src = files.templates.dstdir+files.templates.name;
+	log("   ["+src+"]");
+	return del.sync(src);
 });
 
 gulp.task('html:clean', function() {
-	return del.sync(files.html.prod.dest+'/'+files.html.name);
+	var src = files.html.prod.dest+'/'+files.html.name;
+	log("   ["+src+"]");
+	return del.sync(src);
 });
 
 gulp.task('devhtml:clean', function() {
-	return del.sync(files.html.dev.dest+'/'+files.html.name);
+	var src = files.html.dev.dest+'/'+files.html.name;
+	log("   ["+src+"]");
+	return del.sync(src);
 });
 
 gulp.task('devhtml', ['devhtml:clean'], function() {
+	var src = files.html.src;
+	log("   ["+src+"]");
 	return gulp
-		.src(files.html.src)
+		.src(src)
 		.pipe(plugins.rename(files.html.name))
 		.pipe(plugins.preprocess(files.html.dev.preprocess))
 		.pipe(gulp.dest(files.html.dev.dest));
 });
 
 gulp.task('pagination', function() {
-	var src = [].concat(files.js.src, files.js.excludes);
+	var src = cmpdir+'/angularUtils-pagination/dirPagination.js';
+	log("   ["+src+"]");
 	return gulp
-		.src(cmpdir+'/angularUtils-pagination/dirPagination.js')
+		.src(src)
 		.pipe(plugins.rename('dirPagination.min.js'))
 		.pipe(plugins.uglify())
 		.pipe(gulp.dest(cmpdir+'/angularUtils-pagination'));
@@ -158,6 +167,7 @@ gulp.task('pagination', function() {
 
 gulp.task('jshint', ['pagination'], function() {
 	var src = [].concat(files.js.src, files.e2e.src, '!'+srcdir+'/**'+files.templates.name, '!'+srcdir+'/**/'+dataname);
+	log("   ["+src+"]");
 	return gulp
 		.src(src)
 		.pipe(plugins.iife())
@@ -171,35 +181,45 @@ gulp.task('testauto', ['jshint', 'devhtml'], function(done) { karma(done, {autoW
 gulp.task('coverage', ['jshint', 'devhtml'], function(done) { karma(done, {reporters: ['coverage']}); });
 
 gulp.task('cssmin', function() {
+	var src = files.css.src;
+	log("   ["+src+"]");
 	return gulp
-		.src(files.css.src)
+		.src(src)
 		.pipe(plugins.cleanCss())
 		.pipe(plugins.concat(files.css.name))
 		.pipe(gulp.dest(files.css.dest));
 });
 
 gulp.task('img', function() {
+	var src = files.img.src;
+	log("   ["+src+"]");
 	return gulp
-		.src(files.img.src)
+		.src(src)
 		.pipe(gulp.dest(files.img.dest));
 });
 
 gulp.task('csslibs', function() {
+	var src = files.css.libs;
+	log("   ["+src+"]");
 	return gulp
-		.src(files.css.libs)
+		.src(src)
 		.pipe(gulp.dest(files.css.dest));
 });
 
 gulp.task('jslibs', function() {
+	var src = files.js.libs;
+	log("   ["+src+"]");
 	return gulp
-		.src(files.js.libs)
+		.src(src)
 		.pipe(plugins.concat(files.js.libsname, {newLine: ';'}))
 		.pipe(gulp.dest(files.js.dest));
 });
 
 gulp.task('templates', ['templates:clean'], function() {
+	var src = files.templates.src;
+	log("   ["+src+"]");
 	return gulp
-		.src(files.templates.src)
+		.src(src)
 		.pipe(plugins.htmlmin())
 		.pipe(plugins.angularTemplatecache({module: files.templates.module}))
 		.pipe(gulp.dest(files.templates.dest));
@@ -207,6 +227,7 @@ gulp.task('templates', ['templates:clean'], function() {
 
 gulp.task('uglify', ['uglify:clean', 'templates', 'test'], function() {
 	var src = [].concat(files.js.src, files.js.excludes);
+	log("   ["+src+"]");
 	return gulp
 		.src(src)
 		.pipe(plugins.concat(files.js.temp, {newLine: ';'}))
@@ -217,25 +238,37 @@ gulp.task('uglify', ['uglify:clean', 'templates', 'test'], function() {
 			.pipe(plugins.iife())
 			.pipe(plugins.uglify({mangle: false, compress: true}))
 		.pipe(plugins.sourcemaps.write('./'))
+		.pipe(gulp.dest(files.js.dest));
+});
+
+gulp.task('header', ['uglify'], function() {
+	var src = files.js.dest + "/" + files.js.name;
+	log("   ["+src+"]");
+	return gulp
+		.src(src)
 		.pipe(plugins.header(banner, {pkg: pkg}))
 		.pipe(gulp.dest(files.js.dest));
 });
 
 gulp.task('html', ['html:clean'], function() {
+	var src = files.html.src;
+	log("   ["+src+"]");
 	return gulp
-		.src(files.html.src)
+		.src(src)
 		.pipe(plugins.rename(files.html.name))
 		.pipe(plugins.preprocess(files.html.prod.preprocess))
 		.pipe(gulp.dest(files.html.prod.dest));
 });
 
 gulp.task('other', function() {
+	var src = files.other.src;
+	log("   ["+src+"]");
 	return gulp
-		.src(files.other.src)
+		.src(src)
 		.pipe(gulp.dest(files.other.dest));
 });
 
-gulp.task('webserver', ['jslibs', 'uglify', 'csslibs', 'cssmin', 'html', 'img', 'other'], function() {
+gulp.task('webserver', ['jslibs', 'header', 'csslibs', 'cssmin', 'html', 'img', 'other'], function() {
 	plugins.connect.server({
 		root: dstdir,
 		port: 8080
@@ -245,18 +278,23 @@ gulp.task('webserver', ['jslibs', 'uglify', 'csslibs', 'cssmin', 'html', 'img', 
 gulp.task('webdriverUpdate', plugins.protractor.webdriver_update);
 
 gulp.task('e2e', ['webdriverUpdate', 'webserver'], function() {
+	var src = '../'+files.e2e.src;
+	log("   ["+src+"]");
 	return gulp
-		.src('../'+files.e2e.src)
+		.src(src)
 		.pipe(plugins.protractor.protractor({configFile: 'config/protractor.conf.js'}));
 });
 
 gulp.task('build', ['e2e'], function() {
+	var src = files.js.dest+'/'+files.js.temp;
+	log("   ["+src+"]");
 	plugins.connect.serverClose();
-	return del(files.js.dest+'/'+files.js.temp);
+	return del(src);
 });
 
 gulp.task('watch', function() {
 	var src = [].concat(files.main.src, files.test.src, files.e2e.src);
+	log("   ["+src+"]");
 	gulp.watch(src, ['jshint']);
 });
 
@@ -282,19 +320,25 @@ gulp.task('version:minor', function() { return version('minor'); });
 gulp.task('version:major', function() { return version('major'); });
 
 gulp.task('git:tag', function() {
-	return gulp.src(files.version.src)
+	var src = files.version.src;
+	log("   ["+src+"]");
+	return gulp.src(src)
 		.pipe(plugins.git.commit(files.version.message))
 		.pipe(plugins.filter(files.version.filter))
 		.pipe(plugins.tagVersion());
 });
 
 gulp.task('release:clean', function() {
-	return del.sync(files.release.dest);
+	var src = files.release.dest;
+	log("   ["+src+"]");
+	return del.sync(src);
 });
 
 gulp.task('release', ['release:clean', 'build'], function() {
+	var src = files.release.src;
+	log("   ["+src+"]");
 	return gulp
-		.src(files.release.src)
+		.src(src)
 		.pipe(plugins.tar(files.release.name))
 		.pipe(plugins.gzip({gzipOptions: {level: 9}}))
 		.pipe(gulp.dest(files.release.dest));
