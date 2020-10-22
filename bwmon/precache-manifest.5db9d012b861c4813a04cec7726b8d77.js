4bd5459b21c86904e3d412c502444c95abbc8c11
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2020-10-22 10:18:41 -0400

    new demo and coverage

---
+++ bwmon/precache-manifest.5db9d012b861c4813a04cec7726b8d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f3617b6f2cec4872a4289e707df7b3",
+    "url": "/bwmon/index.html"
+  },
+  {
+    "revision": "f9f832569a9a24c30324",
+    "url": "/bwmon/static/js/2.b8e84995.chunk.js"
+  },
+  {
+    "revision": "b628150e23f8915b07e1d67cfe828785",
+    "url": "/bwmon/static/js/2.b8e84995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf0447396227b6f0b01",
+    "url": "/bwmon/static/js/main.58bf7cca.chunk.js"
+  },
+  {
+    "revision": "e55be89a4ae2742f30d1",
+    "url": "/bwmon/static/js/runtime-main.cd2340ef.js"
+  }
+]);
