6019addf59ef4649e560bbc2da56f5cbfbb431ed
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2025-02-14 10:29:22 -0500

    cleanup

--- server/bwmon.sh
+++ server/bwmon.sh
@@ -19,12 +19,12 @@
 #!/bin/sh
 
 display() {
-	echo "Usage : $0 {setup|update|publish} path"
-	echo "Parameters: "
-	echo "	$0 setup path"
-	echo "	$0 update path"
-	echo "	$0 publish path"
-	echo "Note: bc must be installed"
+  echo "Usage : $0 {setup|update|publish} path"
+  echo "Parameters: "
+  echo "  $0 setup path"
+  echo "  $0 update path"
+  echo "  $0 publish path"
+  echo "Note: bc must be installed"
 }
 
 [ -z "${1}" ] && display && exit 1
@@ -48,152 +48,153 @@ DESTDB=/opt/share/www/bwmon/${OUTPUT_DB}
 DESTLOG=/opt/var/log/bwmon.log
 
 log() {
-	echo $(date) $USER $0 "$1" >> ${DESTLOG}
-	echo $(date) $USER $0 "$1"
+  echo $(date) $USER $0 "$1" >> ${DESTLOG}
+  echo $(date) $USER $0 "$1"
 }
 
 lock() {
-	while [ -f /tmp/bwmon.lock ]; do
-		if [ ! -d /proc/$(cat /tmp/bwmon.lock) ]; then
-			log "WARNING : Lockfile detected but process $(cat /tmp/bwmon.lock) does not exist !"
-			rm -f /tmp/bwmon.lock
-		fi
-		sleep 1
-	done
-	echo $$ > /tmp/bwmon.lock
+  while [ -f /tmp/bwmon.lock ]; do
+    if [ ! -d /proc/$(cat /tmp/bwmon.lock) ]; then
+      log "WARNING : Lockfile detected but process $(cat /tmp/bwmon.lock) does not exist !"
+      rm -f /tmp/bwmon.lock
+    fi
+    sleep 1
+  done
+  echo $$ > /tmp/bwmon.lock
 }
 
 unlock() {
-	rm -f /tmp/bwmon.lock
+  rm -f /tmp/bwmon.lock
 }
 
 usage() {
-	# publish results
-	# header
-	echo "/*Bandwidth Monitor Data*/" > ${OUTPUTDB}
-	# details
-	echo "$(echo ${MACNAMES/'macnames='/} | tr '<' '\n' | tr '>' ',')" > ${MACNAMESFILE}
-	cat ${USAGEDB} | while IFS=, read CURRENT_MONTH MAC IP USAGE_IN USAGE_OUT CREATE_TIME UPDATE_TIME
-	do
-		MACU=$(echo "$MAC" | tr '[a-z]' '[A-Z')
-		USER=$(grep "${IP} " "${USERSFILE}" | cut -f2 -s -d' ' )
-		if [ -z "$USER" ]; then
-			USER=$(grep "${MAC}" "${MACNAMESFILE}" | cut -f1 -s -d',' )
-			[ -z "$USER" ] && USER=${MAC}
-		fi
-		echo "${CURRENT_MONTH},${IP},${MACU},${USER},${USAGE_IN},${USAGE_OUT},${CREATE_TIME},${UPDATE_TIME}" >> ${OUTPUTDB}
-	done
-	# footer
-	mv -f ${OUTPUTDB} ${DESTDB}
+  # publish results
+  # header
+  echo "/*Bandwidth Monitor Data*/" > ${OUTPUTDB}
+  # details
+  echo "$(echo ${MACNAMES/'macnames='/} | tr '<' '\n' | tr '>' ',')" > ${MACNAMESFILE}
+  cat ${USAGEDB} | while IFS=, read CURRENT_MONTH MAC IP USAGE_IN USAGE_OUT CREATE_TIME UPDATE_TIME
+  do
+    MACU=$(echo "$MAC" | tr '[a-z]' '[A-Z')
+    USER=$(grep "${IP} " "${USERSFILE}" | cut -f2 -s -d' ' )
+    if [ -z "$USER" ]; then
+      USER=$(grep "${MAC}" "${MACNAMESFILE}" | cut -f1 -s -d',' )
+      [ -z "$USER" ] && USER=${MAC}
+    fi
+    echo "${CURRENT_MONTH},${IP},${MACU},${USER},${USAGE_IN},${USAGE_OUT},${CREATE_TIME},${UPDATE_TIME}" >> ${OUTPUTDB}
+  done
+  # footer
+  mv -f ${OUTPUTDB} ${DESTDB}
 }
 
 update() {
-	# Read and reset counters
-	iptables -L RRDIPT -vnxZ -t filter > /tmp/traffic_$$.tmp
-	grep -v "0x0" /proc/net/arp  | while read IP TYPE FLAGS MAC MASK IFACE
-	do
-		MACU=$(echo "$MAC" | tr '[a-z]' '[A-Z')
-		echo 0 > /tmp/in_$$.tmp
-		echo 0 > /tmp/out_$$.tmp
-		grep ${IP} /tmp/traffic_$$.tmp | while read PKTS BYTES TARGET PROT OPT IFIN IFOUT SRC DST
-		do
-			[ "${DST}" = "${IP}" ] && echo ${BYTES}/1000 | bc > /tmp/in_$$.tmp
-			[ "${SRC}" = "${IP}" ] && echo ${BYTES}/1000 | bc > /tmp/out_$$.tmp
-		done
-		IN=$(cat /tmp/in_$$.tmp)
-		OUT=$(cat /tmp/out_$$.tmp)
-		if [ ${IN} -gt 0 -o ${OUT} -gt 0 ];  then
-			# log "DEBUG: New traffic for ${MACU} since last update : ${IN}k:${OUT}k"
-			CURRENT_MONTH=$(date +'%Y-%m')
-			LINE=$(grep "${CURRENT_MONTH},${MACU}" ${USAGEDB})
-			if [ -z "${LINE}" ]; then
-				log "DEBUG: ${CURRENT_MONTH},${MACU} new month and host!"
-				USAGE_IN=0
-				USAGE_OUT=0
-				CREATE_TIME=$(date +%s)
-			else
-				USAGE_IN=$(echo ${LINE} | cut -f4 -s -d, )
-				USAGE_OUT=$(echo ${LINE} | cut -f5 -s -d, )
-				CREATE_TIME=$(echo ${LINE} | cut -f6 -s -d, )
-			fi
-			# log "DEBUG: Usage before: ${USAGE_IN}k:${USAGE_OUT}k"
-			USAGE_IN=`echo ${USAGE_IN}+${IN} | bc`
-			USAGE_OUT=`echo ${USAGE_OUT}+${OUT} | bc`
-			# log "DEBUG: Usage after : ${USAGE_IN}k:${USAGE_OUT}k"
-			UPDATE_TIME=$(date +%s)
-			grep -v "${CURRENT_MONTH},${MACU}" ${USAGEDB} > /tmp/db_$$.tmp
-			mv /tmp/db_$$.tmp ${USAGEDB}
-			echo ${CURRENT_MONTH},${MACU},${IP},${USAGE_IN},${USAGE_OUT},${CREATE_TIME},${UPDATE_TIME} >> ${USAGEDB}
-		fi
-	done
-	rm -f /tmp/*_$$.tmp
+  # Read and reset counters
+  iptables -L RRDIPT -vnxZ -t filter > /tmp/traffic_$$.tmp
+  grep -v "0x0" /proc/net/arp  | while read IP TYPE FLAGS MAC MASK IFACE
+  do
+    MACU=$(echo "$MAC" | tr '[a-z]' '[A-Z')
+    echo 0 > /tmp/in_$$.tmp
+    echo 0 > /tmp/out_$$.tmp
+    grep ${IP} /tmp/traffic_$$.tmp | while read PKTS BYTES TARGET PROT OPT IFIN IFOUT SRC DST
+    do
+      [ "${DST}" = "${IP}" ] && echo ${BYTES}/1000 | bc > /tmp/in_$$.tmp
+      [ "${SRC}" = "${IP}" ] && echo ${BYTES}/1000 | bc > /tmp/out_$$.tmp
+    done
+    IN=$(cat /tmp/in_$$.tmp)
+    OUT=$(cat /tmp/out_$$.tmp)
+    if [ ${IN} -gt 0 -o ${OUT} -gt 0 ];  then
+      # log "DEBUG: New traffic for ${MACU} since last update : ${IN}k:${OUT}k"
+      CURRENT_MONTH=$(date +'%Y-%m')
+      LINE=$(grep "${CURRENT_MONTH},${MACU}" ${USAGEDB})
+      if [ -z "${LINE}" ]; then
+        log "DEBUG: ${CURRENT_MONTH},${MACU} new month and host!"
+        USAGE_IN=0
+        USAGE_OUT=0
+        CREATE_TIME=$(date +%s)
+      else
+        USAGE_IN=$(echo ${LINE} | cut -f4 -s -d, )
+        USAGE_OUT=$(echo ${LINE} | cut -f5 -s -d, )
+        CREATE_TIME=$(echo ${LINE} | cut -f6 -s -d, )
+      fi
+      # log "DEBUG: Usage before: ${USAGE_IN}k:${USAGE_OUT}k"
+      USAGE_IN=`echo ${USAGE_IN}+${IN} | bc`
+      USAGE_OUT=`echo ${USAGE_OUT}+${OUT} | bc`
+      # log "DEBUG: Usage after : ${USAGE_IN}k:${USAGE_OUT}k"
+      UPDATE_TIME=$(date +%s)
+      grep -v "${CURRENT_MONTH},${MACU}" ${USAGEDB} > /tmp/db_$$.tmp
+      mv /tmp/db_$$.tmp ${USAGEDB}
+      echo ${CURRENT_MONTH},${MACU},${IP},${USAGE_IN},${USAGE_OUT},${CREATE_TIME},${UPDATE_TIME} >> ${USAGEDB}
+    fi
+  done
+  rm -f /tmp/*_$$.tmp
 }
 
 create() {
-	# Create the RRDIPT CHAIN (it doesn't matter if it already exists).
-	iptables -N RRDIPT 2> /dev/null
-	# Add the RRDIPT CHAIN to the FORWARD chain (if non existing).
-	iptables -L FORWARD --line-numbers -n | grep "RRDIPT" | grep "1" > /dev/null
-	if [ $? -ne 0 ]; then
-		iptables -L FORWARD -n | grep "RRDIPT" > /dev/null
-		if [ $? -eq 0 ]; then
-			log "DEBUG : iptables chain misplaced, recreating it..."
-			iptables -D FORWARD -j RRDIPT
-		fi
-		iptables -I FORWARD -j RRDIPT
-	fi
-	# LAN0 For each host in the ARP table
-	grep ${LAN0_IFACE} /proc/net/arp | while read IP TYPE FLAGS MAC MASK IFACE
-	do
-		# Add iptable rules (if non existing).
-		iptables -nL RRDIPT | grep "${IP} " > /dev/null
-		if [ $? -ne 0 ]; then
-			iptables -I RRDIPT -d ${IP} -j RETURN
-			iptables -I RRDIPT -s ${IP} -j RETURN
-		fi
-	done
-	# LAN1 For each host in the ARP table
-	grep ${LAN1_IFACE} /proc/net/arp | while read IP TYPE FLAGS MAC MASK IFACE
-	do
-		# Add iptable rules (if non existing).
-		iptables -nL RRDIPT | grep "${IP} " > /dev/null
-		if [ $? -ne 0 ]; then
-			iptables -I RRDIPT -d ${IP} -j RETURN
-			iptables -I RRDIPT -s ${IP} -j RETURN
-		fi
-	done
+  # Create the RRDIPT CHAIN (it doesn't matter if it already exists).
+  iptables -N RRDIPT 2> /dev/null
+  # Add the RRDIPT CHAIN to the FORWARD chain (if non existing).
+  iptables -L FORWARD --line-numbers -n | grep "RRDIPT" | grep "1" > /dev/null
+  if [ $? -ne 0 ]; then
+    iptables -L FORWARD -n | grep "RRDIPT" > /dev/null
+    if [ $? -eq 0 ]; then
+      log "DEBUG : iptables chain misplaced, recreating it..."
+      iptables -D FORWARD -j RRDIPT
+    fi
+    iptables -I FORWARD -j RRDIPT
+  fi
+  # LAN0 For each host in the ARP table
+  grep ${LAN0_IFACE} /proc/net/arp | while read IP TYPE FLAGS MAC MASK IFACE
+  do
+    # Add iptable rules (if non existing).
+    iptables -nL RRDIPT | grep "${IP} " > /dev/null
+    if [ $? -ne 0 ]; then
+      iptables -I RRDIPT -d ${IP} -j RETURN
+      iptables -I RRDIPT -s ${IP} -j RETURN
+    fi
+  done
+  # LAN1 For each host in the ARP table
+  grep ${LAN1_IFACE} /proc/net/arp | while read IP TYPE FLAGS MAC MASK IFACE
+  do
+    # Add iptable rules (if non existing).
+    iptables -nL RRDIPT | grep "${IP} " > /dev/null
+    if [ $? -ne 0 ]; then
+      iptables -I RRDIPT -d ${IP} -j RETURN
+      iptables -I RRDIPT -s ${IP} -j RETURN
+    fi
+  done
 }
 
 fcdisable() {
-	fc status > /dev/null 2>&1
-	if [ $? -eq 0 ]; then
-    	log "Flow Cache will be disabled ..."
-    	fc disable
-	   	fc flush
-	fi
+  fc status > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    log "Flow Cache will be disabled ..."
+    fc disable
+    fc flush
+  fi
 }
 
 case ${1} in
 "setup" )
-	create
-	;;
+  create
+  ;;
 "update" )
-	log "INFO: Running update - ${USAGEDB} ${OUTPUTDB} ${USERSFILE}"
-	lock
-	update
-	usage
-	unlock
-	;;
+  log "INFO: Running update - ${USAGEDB} ${OUTPUTDB} ${USERSFILE}"
+  fcdisable
+  lock
+  update
+  usage
+  unlock
+  ;;
 "publish" )
-	log "INFO: Running publish - ${USAGEDB} ${OUTPUTDB} ${USERSFILE}"
-	lock
-	usage
-	unlock
-	;;
+  log "INFO: Running publish - ${USAGEDB} ${OUTPUTDB} ${USERSFILE}"
+  lock
+  usage
+  unlock
+  ;;
 *)
-	display
-	exit 1
-	;;
+  display
+  exit 1
+  ;;
 esac
 
 exit 0 
