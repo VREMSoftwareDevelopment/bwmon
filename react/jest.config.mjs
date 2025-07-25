dda6b733d63b41f7f145667a635527bbc96dba71
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2025-07-25 13:48:52 -0400

    update dependencies

--- react/jest.config.mjs
+++ react/jest.config.mjs
@@ -6,6 +6,8 @@ export default {
     transform: {
         '^.+\\.[t|j]sx?$': 'babel-jest',
     },
+    // Allow transformation of ESM modules in node_modules, except for those that don't need it
+    transformIgnorePatterns: ['/node_modules/(?!(react-error-boundary)/)'],
     collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/index.jsx', '!src/serviceWorker.js', '!**/e2e/**', '!**/__mocks__/**'],
     coverageDirectory: '<rootDir>/reports/coverage',
     coverageThreshold: {
