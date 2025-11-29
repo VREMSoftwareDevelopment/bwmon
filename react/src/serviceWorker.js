4953aad9c988b5ec1d9989412428461877079b35
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2025-11-29 09:59:25 -0500

    minor change

--- react/src/serviceWorker.js
+++ react/src/serviceWorker.js
@@ -12,10 +12,10 @@
 
 const isLocalhost = Boolean(
     window.location.hostname === 'localhost' ||
-        // [::1] is the IPv6 localhost address.
-        window.location.hostname === '[::1]' ||
-        // 127.0.0.0/8 are considered localhost for IPv4.
-        window.location.hostname.startsWith('127.')
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
+    window.location.hostname.startsWith('127.')
 );
 
 export function register(config) {
